a7ab96ff9d885d8c251bf76589a4b7a4c06322fa
halilbahar <halil.s.bahar@gmail.com>
2020-12-07 00:45:17 +0100

    Create a class service which manages the classes.json file

---
+++ src/app/core/services/class.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Class } from 'src/app/shared/models/class.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassService {
+
+  private classes: Class[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.readClasses();
+  }
+
+  /**
+   * Read the local classes.json file from the assets folder
+   */
+  async readClasses(): Promise<void> {
+    this.classes = await this.http.get<Class[]>('assets/classes.json').toPromise();
+  }
+
+  /**
+   * Get all classes that were read from the classes.json file
+   */
+  getClasses(): Class[] {
+    return this.classes;
+  }
+
+  /**
+   * Find a class by year, name and department from the classes.json file
+   * 
+   * @param year of the class
+   * @param name of the class
+   * @param department  of the class
+   * 
+   * @returns the class or null
+   */
+  getClassByName(year: number, name: string, department: string): Class | null {
+    const clazz = this.classes.find(clazz => clazz.year === year && clazz.name === name && clazz.department === department);
+    return clazz || null;
+  }
+}
