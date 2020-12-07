cd7f119b125b76333554a7467af6c282e4517208
halilbahar <halil.s.bahar@gmail.com>
2020-12-07 03:25:07 +0100

    Display page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClassListComponent } from './pages/class-list/class-list.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', component: ClassListComponent }
+  { path: '', component: ClassListComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
