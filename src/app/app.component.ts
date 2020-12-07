50e33796aa74038bfcb6677c6dd88d42f8012b2f
halilbahar <halil.s.bahar@gmail.com>
2020-12-07 02:10:20 +0100

    Clean up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubGraphqlApiService } from './core/services/github-graphql-api.service';
-import { Class } from './shared/models/class.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'rip-rep';
-
-  constructor(
-    private graphql: GithubGraphqlApiService
-  ) { }
-
-  ngOnInit(): void {
-    this.graphql.getStudentsData({} as Class);
-  }
 }
