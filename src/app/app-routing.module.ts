9139a081375aa884de1738bad1be3af576eff5f7
lorenzgruber <regru.lg@gmail.com>
2020-12-10 00:19:16 +0100

    implement breadcrumbs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ClassListComponent } from './pages/class-list/class-list.component';
-import { ClassStatisticComponent } from './pages/class-statistic/class-statistic.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { UserQueryResolver } from './core/resolver/user-query.resolver';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ClassListComponent} from './pages/class-list/class-list.component';
+import {ClassStatisticComponent} from './pages/class-statistic/class-statistic.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {UserQueryResolver} from './core/resolver/user-query.resolver';
 
 const routes: Routes = [
   { path: '', component: ClassListComponent },
@@ -15,4 +15,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivationEnd, NavigationEnd, Params, Router} from '@angular/router';
+import {Breadcrumb} from '../../shared/models/breadcrumb.model';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  breadcrumbs: Breadcrumb[] = [];
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    let segments: { path: string; params: Params, usePathParamAsTitle: boolean }[] = [];
+
+    this.router.events.subscribe(event => {
+      if (event instanceof ActivationEnd) {
+        const route = event.snapshot;
+        segments.unshift({
+          // @ts-ignore
+          path: route.routeConfig.path,
+          params: route.params,
+          usePathParamAsTitle: route.data.usePathParamAsTitle
+        });
+      }
+
+      if (event instanceof NavigationEnd) {
+        const breadcrumbs: Breadcrumb[] = [];
+
+        let link = '';
+        for (const segment of segments) {
+          let path = segment.path;
+          if (path === '') {
+            continue;
+          }
+
+
+          const pathParamValues: string[] = [];
+          // Itterate over the object that contains the path param and
+          // replace the placeholder with the actual value: /path/:id => /path/1
+          const params = segment.params;
+          for (const key in params) {
+            if (params.hasOwnProperty(key)) {
+              const value = segment.params[key];
+              pathParamValues.push(value);
+              path = path.replace(`:${key}`, value);
+            }
+          }
+
+          // Use the values /path/5ahitm => 5ahitm or remove any path param associated with the path: /path/:id => /path
+          const title = segment.usePathParamAsTitle ? pathParamValues.join(' ') : path.replace(/\/:\w+/, '');
+
+          link += `/${path}`;
+          breadcrumbs.push({link, title});
+        }
+
+        this.breadcrumbs = breadcrumbs;
+        segments = [];
+      }
+    });
   }
 
 }
