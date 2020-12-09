d9788131783da91a2e6cd9a7d262713935692c42
lorenzgruber <regru.lg@gmail.com>
2020-12-10 00:19:33 +0100

    coding help from mr. halil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.compo
 import {UserQueryResolver} from './core/resolver/user-query.resolver';
 
 const routes: Routes = [
-  { path: '', component: ClassListComponent },
-  { path: 'class/:name', component: ClassStatisticComponent, resolve: { userQueries: UserQueryResolver } },
-  { path: '**', component: PageNotFoundComponent }
+  {path: '', component: ClassListComponent},
+  {path: 'class/:name', component: ClassStatisticComponent, resolve: {userQueries: UserQueryResolver}, data: {usePathParamAsTitle: true}},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
