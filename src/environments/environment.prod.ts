24540fc8d6d9611186a1d5a5be06739953321ae5
halilbahar <halil.s.bahar@gmail.com>
2020-12-10 22:12:50 +0100

    Setup prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: '3ff386b5c575af2e1a4d',
+  client_secret: 'ec1abca366fb57b7b529aba076f75deefa826c9f',
+  redirect_uri: 'https://halilbahar.github.io/rip-rep'
 };
