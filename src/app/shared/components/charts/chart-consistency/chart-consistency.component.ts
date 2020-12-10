f4fc0b4460c086763e8aa431fc1a457b142841fe
Lorenz <regru.lg@gmail.com>
2020-12-10 15:14:19 +0100

    implement consistency chart

--- src/app/shared/components/charts/chart-consistency/chart-consistency.component.ts
+++ src/app/shared/components/charts/chart-consistency/chart-consistency.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {UserQuery} from '../../../models/user-query.model';
+import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
+import {Label} from 'ng2-charts';
 
 @Component({
   selector: 'app-chart-consistency',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartConsistencyComponent implements OnInit {
 
-  constructor() { }
+  @Input() userQueries: UserQuery[];
 
-  ngOnInit(): void {
+  barChartOptions: ChartOptions = {responsive: true, maintainAspectRatio: false};
+  barChartLabels: Label[] = [];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartData: ChartDataSets[] = [];
+
+  days = 30;
+
+  constructor() {
   }
 
+  ngOnInit(): void {
+    type UserConsistency = { username: string, consistency: number };
+    const users: UserConsistency[] = [];
+
+    for (const query of this.userQueries) {
+      const weeks = query.user.contributionsCollection.contributionCalendar.weeks;
+      let count = 0;
+      let daysCommited = 0;
+
+      for (const week of weeks) {
+        for (const day of week.contributionDays) {
+          if (count === this.days) {
+            break;
+          }
+          if (query.user.login === 'halilbahar') {
+            console.log(day.contributionCount);
+          }
+          if (day.contributionCount > 0) {
+            daysCommited++;
+          }
+          count++;
+        }
+      }
+
+      const consistency = (daysCommited / count * 100).toFixed(2);
+
+      users.push({
+        username: query.user.login,
+        consistency
+      });
+    }
+
+    // Get top 5
+    users.sort((a, b) => b.consistency - a.consistency);
+    const topFiveUsers = users.slice(0, 5);
+
+    topFiveUsers.forEach(user => this.barChartLabels.push(user.username));
+    this.barChartData.push({
+      data: [...topFiveUsers.map(user => user.consistency)],
+      label: 'Commit Consistency'
+    });
+  }
 }
