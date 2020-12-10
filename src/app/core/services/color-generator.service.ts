f6b902663f5c7fd80aa8fdf75bdc0f5e309b135d
Lorenz <regru.lg@gmail.com>
2020-12-10 19:50:44 +0100

    fix review issues

--- src/app/core/services/color-generator.service.ts
+++ src/app/core/services/color-generator.service.ts
@@ -8,11 +8,10 @@ export class ColorGeneratorService {
   constructor() {
   }
 
-  getColor(amount = 1): string | string[] {
+  getColor(amount): string[] {
     const colors = [];
 
     for (let i = 0; i < amount; i++) {
-
       colors.push(this.hsl2hex(
         Math.round(Math.random() * 360),
         Math.round(80 - Math.random() * 10) / 100,
@@ -20,18 +19,11 @@ export class ColorGeneratorService {
       ));
     }
 
-    console.log(colors);
-
-    if (colors.length === 1) {
-      return colors[0];
-    }
-
     return colors;
   }
 
   // code derived from https://stackoverflow.com/questions/36721830/convert-hsl-to-rgb-and-hex
   private hsl2hex(h: number, s: number, l: number): string {
-    console.log(`${h},${s},${l}`);
     const a = s * Math.min(l, 1 - l);
     const f = (n: number, k: number = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
     return '#' + [f(0), f(8), f(4)].map(x => Math.round(x * 255).toString(16).padStart(2, '0')).join('');
