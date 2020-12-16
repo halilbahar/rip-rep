56e299a6d3d14a70a514b1887e04e49624f29383
halilbahar <halil.s.bahar@gmail.com>
2020-12-16 23:15:45 +0100

    Implement the total commits

--- src/app/shared/components/charts/chart-all-commits/chart-all-commits.component.ts
+++ src/app/shared/components/charts/chart-all-commits/chart-all-commits.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { UserQuery } from 'src/app/shared/models/user-query.model';
 
 @Component({
   selector: 'app-chart-all-commits',
@@ -7,9 +10,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartAllCommitsComponent implements OnInit {
 
-  constructor() { }
+  @Input() userQueries: UserQuery[];
+
+  lineChartOptions: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+      position: 'nearest',
+      filter: function (item, data) {
+        const dataset = data.datasets![item.datasetIndex!];
+        const dataValue = dataset.data![item.index!]!;
+        return dataValue <= 0 ? false : true;
+      },
+      callbacks: {
+        title: () => ''
+      }
+    },
+    elements: {
+      line: {
+        tension: 0
+      }
+    }
+  };
+  lineChartLabels: Label[] = [];
+  lineChartType: ChartType = 'line';
+  lineChartLegend = true;
+  lineChartData: ChartDataSets[] = [];
+  lineChartColors: Color[] = [];
 
   ngOnInit(): void {
-  }
+    type StudentContributionDay = {
+      login: string,
+      data: {
+        contributionCount: number,
+        date: Date
+      }[]
+    };
 
+    const studentContributions: StudentContributionDay[] = [];
+
+
+    for (const query of this.userQueries) {
+      const weeks = query.user.contributionsCollection.contributionCalendar.weeks;
+      const reversedWeeks = [...weeks].reverse();
+      let counter = 0;
+
+      const contribution: StudentContributionDay = {
+        login: query.user.login,
+        data: []
+      };
+
+      // Get the past 30 days. Start from the current day (today)
+      for (const week of reversedWeeks) {
+        const reversedDays = [...week.contributionDays].reverse();
+        for (const day of reversedDays) {
+          if (counter >= 30) {
+            break;
+          }
+
+          contribution.data.push({
+            contributionCount: day.contributionCount,
+            date: new Date(day.date)
+          });
+
+          counter++;
+        }
+      }
+
+      contribution.data.reverse();
+      studentContributions.push(contribution);
+    }
+
+    // Get the past 30 days in day-month format
+    const labels = [];
+    const now = new Date();
+    for (let i = 0; i < 30; i++) {
+      labels.push(now.toLocaleDateString(navigator.language, { day: 'numeric', month: 'numeric' }));
+      now.setDate(now.getDate() - 1);
+    }
+
+    labels.reverse();
+    this.lineChartLabels.push(...labels);
+
+    // map the array so it is compatible with the chart
+    this.lineChartData.push(...studentContributions.map(student => {
+      const data = student.data.map(data => data.contributionCount);
+      return { data, label: student.login }
+    }));
+  }
 }
